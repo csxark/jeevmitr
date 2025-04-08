aa3be6ba5afd853e342fb83f3a781b7299621796
csxark <arktandoncs@gmail.com>
2025-04-08 18:21:26 +0530

    new features

---
+++ script.js
@@ -0,0 +1,16 @@
+function openTab(tabName, event) {
+  var i, tabContent, tabButtons;
+
+  tabContent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].classList.remove("active");
+  }
+
+  tabButtons = document.getElementsByClassName("tab-button");
+  for (i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].classList.remove("active");
+  }
+
+  document.getElementById(tabName).classList.add("active");
+  event.currentTarget.classList.add("active");
+}
